a83320a982413482a8169a072c8ed443bf96bc98
firew1995 <59594233+firew1995@users.noreply.github.com>
2020-04-14 22:51:25 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+ subscription_id = "5582d9fc-4b34-4ec0-8d03-5df89d023ffe"
+ client_id = "ae836f09-931b-4e6b-af13-ddc426c0ecdc"
+ client_secret = "eWBS/agdw4di.RYS?QXg?iuZrmyfF154"
+ tenant_id = "037d95db-f9ae-4d6b-b2c3-094fcce3858b"
+ location = "east us"
+  
+}
+
+resource "azurerm_resource_group" "resource_gp" {
+ name = "Terraform_rg"
+ }
