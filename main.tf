319188eeeda20471cf72fb3205893078c98039ec
firew1995 <59594233+firew1995@users.noreply.github.com>
2020-04-14 23:56:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 provider "azurerm" {
  subscription_id = "5582d9fc-4b34-4ec0-8d03-5df89d023ffe"
  client_id = "ae836f09-931b-4e6b-af13-ddc426c0ecdc"
- client_secret = "eWBS/agdw4di.RYS?QXg?iuZrmyfF154"
+ client_secret = "P_xFNvVtD-eA503Xb2]BPfzg@e[uEZ=w"
  tenant_id = "037d95db-f9ae-4d6b-b2c3-094fcce3858b"
-}
-
-resource "azurerm_resource_group" "example" {
- name = "Terraform_rg"
- location = "eastus"
+ 
  }
+ 
+resource "azurerm_resource_group" "example" {
+  name     = "Terraform_rg"
+  location = "eastus"
+  tags = {
+    environment = "Sandbox"
+  }
+}
